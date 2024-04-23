bb935f7aad998786d8cc91bf0bb34e28ba73579a
olehdenysenko <olehden20@gmail.com>
2024-04-23 17:16:02 +0900

    convert buy/sell fee to eth

--- contract/contracts/SAPE.sol
+++ contract/contracts/SAPE.sol
@@ -1,27 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./interfaces/ISAPE.sol";
 import "./libraries/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
-contract SalchainToken is ISAPE {
+contract SalchainToken is ERC20 {
     using SafeMath for uint256;
 
-    string  public  name;
-    string  public  symbol;
-    uint8   public  decimals;
+    uint8   private  decimals;
     uint256 public  totalSupply_;
 
     mapping(address => uint256) balances;
     mapping(address => mapping (address => uint256)) allowed;
 
-    uint256 buyFee = 2;
-    uint256 sellFee = 5;
-    address swapPair;
+    uint256 private buyFee = 2;
+    uint256 private sellFee = 5;
+    address private feeTo;
+    address private swapPair;
+    address private router;
 
-    constructor() {
-        name = "NFT-Reward-Token";
-        symbol = "SAPE";
+    constructor() ERC20("NFT-Reward-Token", "SAPE") {
         decimals = 18;
         totalSupply_ = 10000000000000000000000;     // total tokens would equal (totalSupply_/10**decimals)=10000
         balances[msg.sender] = totalSupply_;
@@ -42,48 +42,66 @@ contract SalchainToken is ISAPE {
         return balances[tokenOwner];
     }
 
+    function getPairAddress() public view returns (address) {
+        return swapPair;
+    }
+
     function setPairAddress(address pair) public {
         swapPair = pair;
     }
 
-    function transfer(address receiver, uint256 numTokens) public override returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        uint256 feeAmount = 0;
-        if (receiver == swapPair) {
-            feeAmount = numTokens * sellFee / 100;
-        } else if (msg.sender == swapPair) {
-            feeAmount = numTokens * buyFee / 100;
+    function setFeeTo(address _feeToAddr) public {
+        feeTo = _feeToAddr;
+    }
+
+    function setRouter(address _routerAddr) public {
+        router = _routerAddr;
+    }
+
+    function _transfer(
+        address sender_,
+        address recipient_,
+        uint256 amount_
+    ) internal virtual override {
+        if (sender_ == address(this)) {
+            super._transfer(sender_, recipient_, amount_);
+        } else {
+            uint256 feeAmount = 0;
+            if (recipient_ == swapPair) {
+                feeAmount = amount_ * sellFee / 100;
+            } else if (sender_ == swapPair) {
+                feeAmount = amount_ * buyFee / 100;
+            }
+            uint amt = amount_ - feeAmount;
+            
+            super._transfer(sender_, address(this), feeAmount);
+
+            if (sender_ != swapPair) {
+                _distributeFee();
+            }
+
+            super._transfer(sender_, recipient_, amt);
         }
-        balances[msg.sender] = balances[msg.sender].sub(numTokens - feeAmount);
-        balances[receiver] = balances[receiver].add(numTokens - feeAmount);
-        emit Transfer(msg.sender, receiver, numTokens - feeAmount);
-        return true;
     }
 
-    function approve(address delegate, uint256 numTokens) public override returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-
-    function allowance(address owner, address delegate) public override view returns (uint) {
-        return allowed[owner][delegate];
-    }
-
-    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-
-        uint256 feeAmount = 0;
-        if (buyer == swapPair) {
-            feeAmount = numTokens * sellFee / 100;
-        } else if (owner == swapPair) {
-            feeAmount = numTokens * buyFee / 100;
+    function _distributeFee() internal {
+        uint amount = balanceOf(address(this));
+        if (amount > 0) {
+            _swapTokensForETH(amount);
         }
-        balances[owner] = balances[owner].sub(numTokens - feeAmount);
-        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens).sub(feeAmount);
-        balances[buyer] = balances[buyer].add(numTokens - feeAmount);
-        emit Transfer(owner, buyer, numTokens);
-        return true;
+    }
+
+    function _swapTokensForETH(uint256 amount_) internal {
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = _weth9;
+        _approve(address(this), address(router), amount_);
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            amount_,
+            0,
+            path,
+            feeTo,
+            block.timestamp
+        );
     }
 }
